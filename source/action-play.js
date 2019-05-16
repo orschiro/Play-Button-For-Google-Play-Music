9b4af9636b527dc2fa18e36dd6d02794c52ae256
orschiro <orschiro@gmail.com>
2019-05-16 13:21:11 +0200

    First release ready for Overcast

--- source/action-play.js
+++ source/action-play.js
@@ -1,6 +1,6 @@
 var result = "Play";
 
-var button = document.querySelector('[data-id="play-pause"]')
+var button = document.querySelector('[id="playpausebutton"]')
 if (button.getAttribute("aria-disabled") != "true") {
     button.click()
     if (!button.getAttribute("class").includes(" playing"))
